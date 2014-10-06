7b1fce06f27bce9a24badc6b0663a4070f702b97
Akbar Taufiq Herlangga <angga@atherlangga.com>
2014-10-06 11:11:58 +0700

    Simplify loading.

--- src/room/room-controller.js
+++ src/room/room-controller.js
@@ -1,43 +1,29 @@
 angular.module('roomModule')
 .controller('RoomController',[
-	'$scope',
-	'user',
-	'_',
+	'$scope', 'user', '_',
 	function($scope, user, _) {
 		$scope.rooms = user.rooms;
 		$scope.currentRoom = null;
 		$scope.currentTopic = null;
-		$scope.loading = {
-			rooms:true,
-			topics:true,
-			comments:true
-		};
 
 		var roomsPromise = user.loadRooms();
-		$scope.loading.rooms = true;
 		roomsPromise.then(function(){
-			$scope.loading.rooms= false;
 			$scope.selectRoom(user.rooms[0].id);
 		});
 
 		$scope.selectRoom = function(id){
+			$scope.currentRoom = user.getRoom(id);
+			$scope.currentTopic = $scope.currentRoom.getTopic($scope.currentRoom.lastActiveTopicId);
+
 			var roomPromise = user.loadRoom(id);
-			$scope.loading.topics = true;
-			$scope.loading.comments = true;
 			roomPromise.then(function(){
-				$scope.loading.topics = false;
-				$scope.currentRoom = user.getRoom(id);
-				$scope.selectTopic($scope.currentRoom.lastActiveTopic.id);
+				$scope.selectTopic($scope.currentRoom.lastActiveTopicId);
 			})
 		}
 
 		$scope.selectTopic = function(id){
-			var commentPromise = user.loadTopic(id);
-			$scope.loading.comments = true;
-			commentPromise.then(function(){
-				$scope.loading.comments = false;
-				$scope.currentTopic = $scope.currentRoom.getTopic(id);
-			})
+			$scope.currentTopic = $scope.currentRoom.getTopic(id);			
+			user.loadTopic(id);
 		}
 
 		$scope.sendComment = function(){
