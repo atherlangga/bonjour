c0c750237d3f984845a75b83cd24d57cbfd872fa
Akbar Taufiq Herlangga <angga@atherlangga.com>
2014-10-22 02:02:37 +0700

    Make ChatroomContentController more ordered.

--- src/chatroom/chatroom-content-controller.js
+++ src/chatroom/chatroom-content-controller.js
@@ -3,33 +3,9 @@ function(app,RColor) {
 	app.controller('ChatroomContentController', ['$scope', '$timeout', 'user', 'connectivityEvent',
 		function($scope, $timeout, user, connectivityEvent) {
 			// "Link" model and view.
-			$scope.rooms = user.rooms;
-			$scope.selected = user.selected;
+			$scope.rooms        = user.rooms;
+			$scope.selected     = user.selected;
 			$scope.currentEmail = user.email;
-
-			// Handle connectivity event.
-			connectivityEvent.addOnlineHandler(function() {
-				// Reload all Rooms.
-
-				// Wait 3 seconds to reload. We need to wait a while
-				// before reloading because the backend library *might*
-				// try to replay our previously-failed requests. In
-				// that case, we need to wait for the data source (most
-				// likely a server) to be done processing.
-				$timeout(function() {
-					user.loadRooms()
-					.then(function() {
-						return $scope.selectRoom(user.selected.room.id,
-							user.selected.topic.id);
-					});
-				}, 3000);
-			});
-
-			// Start loading, and cache first 10 Rooms.
-			user.loadRooms()
-			.then(function(){
-				return $scope.selectRoom(user.rooms[0].id);
-			});
 
 			$scope.selectRoom = function(id, initialTopicId){
 				return user.selectRoom(id)
@@ -55,6 +31,31 @@ function(app,RColor) {
 					return user.markTopicAsRead(id);
 				});
 			}
+
+			// Handle connectivity event.
+			connectivityEvent.addOnlineHandler(function() {
+				// Reload all Rooms when we got back our Internet
+				// connection.
+
+				// Wait 3 seconds to reload. We need to wait a while
+				// before reloading because the backend library *might*
+				// try to replay our previously-failed requests. In
+				// that case, we need to wait for the data source (most
+				// likely a server) to be done processing.
+				$timeout(function() {
+					user.loadRooms()
+					.then(function() {
+						return $scope.selectRoom(user.selected.room.id,
+							user.selected.topic.id);
+					});
+				}, 3000);
+			});
+
+			// Start loading.
+			user.loadRooms()
+			.then(function(){
+				return $scope.selectRoom(user.rooms[0].id);
+			});			
 		}]
 	);
 });
